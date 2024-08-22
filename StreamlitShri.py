2ea26fc74b1c3823a443aede25b1e081223534f5
LoL-2003 <148077211+LoL-2003@users.noreply.github.com>
2024-08-22 19:32:15 +0530

    Update StreamlitShri.py

--- StreamlitShri.py
+++ StreamlitShri.py
@@ -127,6 +127,211 @@
 
 
 
+
+
+
+
+
+
+
+# import streamlit as st
+# import streamlit.components.v1 as components
+
+# def header_footer():
+#     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+    
+#     st.markdown("""
+#     <style>
+#         .navbar-dark .navbar-nav .nav-link {
+#             color: black !important;
+#         }
+#         .navbar-dark .navbar-brand img {
+#             width: 150px;
+#             height: 60px;
+#         }
+#     </style>
+#     <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #DC84F3;">
+#       <div class="container-fluid">
+#         <a class="navbar-brand" href="#">
+#           <img src="https://allthatsinteresting.com/wordpress/wp-content/uploads/2014/02/3D-Gifs-Nightmare.gif" alt="Brand Logo">
+#         </a>
+#         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+#           <span class="navbar-toggler-icon"></span>
+#         </button>
+#         <div class="collapse navbar-collapse" id="collapsibleNavbar">
+#           <ul class="navbar-nav">
+#             <li class="nav-item">
+#               <a class="nav-link" href="https://www.linkedin.com/in/adityapuri10/">Contact Us</a>
+#             </li>
+#             <li class="nav-item">
+#               <a class="nav-link" href="https://github.com/Adi1042003">GitHub</a>
+#             </li>
+#             <li class="nav-item">
+#               <a class="nav-link" href="https://drive.google.com/file/d/1lH_kcpl4t9HbF6uWTAc8PR431c1DzIAl/view?usp=sharing">About Project</a>
+#             </li>
+#           </ul>
+#         </div>
+#       </div>
+#     </nav>
+#     """, unsafe_allow_html=True)
+
+#     hide_st_style = """
+#     <style>
+#     #MainMenu {visibility: hidden;}
+#     footer {visibility: hidden;}
+#     footer:after {content:'Made with ❤️ by ADITYA PURI';visibility: visible;display: block;}
+#     .st-emotion-cache-cio0dv {
+#         padding-left: 20%;
+#         padding-right: 1rem;
+#     }
+#     header {visibility: hidden;}
+#     </style>
+#     """
+#     st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# st.set_page_config(page_title="ADITYA")
+# header_footer()
+
+
+# st.title("Streamlit with Neural Network Style Animation Move/Click using Cursor in the Below space")
+
+# # Simplified HTML and JavaScript for the neural network animation
+# neural_network_animation = """
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#   <meta charset="UTF-8">
+#   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#   <title>Neural Network Animation</title>
+#   <style>
+#   body, html {
+#     margin: 0;
+#     padding: 0;
+#     overflow: hidden;
+#     background-color: #000;
+#   }
+#   #particles-js {
+#     position: absolute;
+#     width: 100%;
+#     height: 100%;
+#   }
+#   </style>
+# </head>
+# <body>
+#   <div id="particles-js"></div>
+#   <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+#   <script>
+#     particlesJS("particles-js", {
+#       "particles": {
+#         "number": {
+#           "value": 80,
+#           "density": {
+#             "enable": true,
+#             "value_area": 800
+#           }
+#         },
+#         "color": {
+#           "value": "#FABC3F"
+#         },
+#         "shape": {
+#           "type": "circle",
+#           "stroke": {
+#             "width": 0,
+#             "color": "#000000"
+#           }
+#         },
+#         "opacity": {
+#           "value": 0.5,
+#           "random": false
+#         },
+#         "size": {
+#           "value": 3,
+#           "random": true
+#         },
+#         "line_linked": {
+#           "enable": true,
+#           "distance": 150,
+#           "color": "#FABC3F",
+#           "opacity": 0.4,
+#           "width": 1
+#         },
+#         "move": {
+#           "enable": true,
+#           "speed": 2,
+#           "direction": "none",
+#           "random": true,
+#           "straight": false,
+#           "out_mode": "out",
+#           "bounce": false,
+#           "attract": {
+#             "enable": false
+#           }
+#         }
+#       },
+#       "interactivity": {
+#         "detect_on": "canvas",
+#         "events": {
+#           "onhover": {
+#             "enable": true,
+#             "mode": "grab"
+#           },
+#           "onclick": {
+#             "enable": true,
+#             "mode": "push"
+#           },
+#           "resize": true
+#         },
+#         "modes": {
+#           "grab": {
+#             "distance": 140,
+#             "line_linked": {
+#               "opacity": 1
+#             }
+#           },
+#           "bubble": {
+#             "distance": 300,
+#             "size": 7,
+#             "duration": 2,
+#             "opacity": 8,
+#             "speed": 3
+#           },
+#           "repulse": {
+#             "distance": 200,
+#             "duration": 0.4
+#           },
+#           "push": {
+#             "particles_nb": 4
+#           },
+#           "remove": {
+#             "particles_nb": 2
+#           }
+#         }
+#       },
+#       "retina_detect": true
+#     });
+#   </script>
+# </body>
+# </html>
+# """
+
+# # Embed the HTML into the Streamlit app
+# components.html(neural_network_animation, height=600)
+
+# st.write("Particles are connected with lines, creating a neural network effect.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import streamlit as st
 import streamlit.components.v1 as components
 
@@ -142,30 +347,17 @@ def header_footer():
             width: 150px;
             height: 60px;
         }
+        .stApp {
+            background: transparent !important;
+        }
+        .st-emotion-cache-cio0dv {
+            padding-left: 20%;
+            padding-right: 1rem;
+            background: rgba(255, 255, 255, 0.8); /* Optional: semi-transparent background */
+            border-radius: 10px; /* Optional: add rounded corners to content area */
+            padding: 20px; /* Optional: add padding to content area */
+        }
     </style>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #DC84F3;">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="https://allthatsinteresting.com/wordpress/wp-content/uploads/2014/02/3D-Gifs-Nightmare.gif" alt="Brand Logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.linkedin.com/in/adityapuri10/">Contact Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://github.com/Adi1042003">GitHub</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://drive.google.com/file/d/1lH_kcpl4t9HbF6uWTAc8PR431c1DzIAl/view?usp=sharing">About Project</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
     """, unsafe_allow_html=True)
 
     hide_st_style = """
@@ -173,10 +365,6 @@ def header_footer():
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     footer:after {content:'Made with ❤️ by ADITYA PURI';visibility: visible;display: block;}
-    .st-emotion-cache-cio0dv {
-        padding-left: 20%;
-        padding-right: 1rem;
-    }
     header {visibility: hidden;}
     </style>
     """
@@ -186,9 +374,9 @@ st.set_page_config(page_title="ADITYA")
 header_footer()
 
 
-st.title("Streamlit with Neural Network Style Animation Move/Click using Cursor in the Below space")
+st.title("Streamlit with Neural Network Style Animation")
 
-# Simplified HTML and JavaScript for the neural network animation
+# Neural Network Animation as Background
 neural_network_animation = """
 <!DOCTYPE html>
 <html lang="en">
@@ -204,9 +392,12 @@ neural_network_animation = """
     background-color: #000;
   }
   #particles-js {
-    position: absolute;
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
+    z-index: -1;
   }
   </style>
 </head>
@@ -308,6 +499,7 @@ neural_network_animation = """
 """
 
 # Embed the HTML into the Streamlit app
-components.html(neural_network_animation, height=600)
+components.html(neural_network_animation, height=0, width=0)
 
 st.write("Particles are connected with lines, creating a neural network effect.")
+
